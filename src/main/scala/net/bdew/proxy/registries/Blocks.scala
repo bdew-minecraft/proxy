0848220750a110d06e6a27d1d50b440bc3e1ec94
bdew <blackdew@gmail.com>
2023-07-22 13:54:28 +0300

    initial 1.20.1 port

--- src/main/scala/net/bdew/proxy/registries/Blocks.scala
+++ src/main/scala/net/bdew/proxy/registries/Blocks.scala
@@ -4,12 +4,14 @@ import net.bdew.lib.managers.BlockManager
 import net.bdew.proxy.{ProxyBlock, ProxyEntity, ProxyItem}
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties
-import net.minecraft.world.level.material.Material
+import net.minecraft.world.level.material.MapColor
 
 object Blocks extends BlockManager(Items) {
-  def proxyProps: Properties = props(Material.STONE)
-    .sound(SoundType.STONE)
-    .strength(2, 8)
+  def proxyProps: Properties =
+    props
+      .mapColor(MapColor.STONE)
+      .sound(SoundType.STONE)
+      .strength(2, 8)
 
   define("proxy", () => new ProxyBlock(proxyProps))
     .withTE(new ProxyEntity(_, _, _))
